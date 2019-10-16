f7a237b3c8e9ee0d5c7b7a2855180e1ef0487f33
jenbuebus <56547815+jenbuebus@users.noreply.github.com>
2019-10-16 23:24:57 +0800

    Update cpp.php

--- compilers/cpp.php
+++ compilers/cpp.php
@@ -38,13 +38,13 @@
 			$out=$out." < ".$filename_in;
 			$output=shell_exec($out);
 		}
-		//echo "<pre>$output</pre>";
+		//echo "$output";
 		echo "$output";
               //echo "<textarea id='div' class=\"form-control\" name=\"output\" rows=\"10\" cols=\"50\">$output</textarea><br><br>";
 	}
 	else if(!strpos($error,"error"))
 	{
-		echo "<pre>$error</pre>";
+		echo "$error";
 		if(trim($input)=="")
 		{
 			$output=shell_exec($out);
@@ -59,7 +59,7 @@
 	}
 	else
 	{
-		echo "<pre>$error</pre>";
+		echo "$error";
 	}
 	exec("del $filename_code");
 	exec("del *.o");
